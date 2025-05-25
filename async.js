dd1afb35c6c68844dbb16cf2185a33f0cc0dc4f4
Umwizer <ruthumwizerwa@gmail.com>
2025-05-25 15:22:12 +0300

    New ins

---
+++ async.js
@@ -0,0 +1,29 @@
+//synchronous(synchronous programming)
+
+setTimeout(() => {
+  console.log("1.Preheat the oven to 350 ℉ ");
+}, 1000);
+setTimeout(() => {
+  console.log("2.Chop Half the chocolate into checks & melt is microwave");
+}, 2000);
+setTimeout(() => {
+  console.timeLog("3.Mix butter & sugar");
+}, 1500);
+setTimeout(() => {
+  console.log("4.Beats in the eggs and vanilla for 1-2 minutes.");
+}, 3000);
+setTimeout(() => {
+  console.log("5.Whisk in the melted Chocolate");
+}, 4000);
+setTimeout(() => {
+  console.log(
+    "6.fold in other half of chocolate chunks, the transfer the butter to baking dish"
+  );
+}, 3500);
+
+setTimeout(() => {
+  console.log("7.Bake for 20-25 minutes, then let cool completely");
+}, 1800);
+setTimeout(() => {
+  console.log("8.Slice,serve & enjoy  !");
+}, 500);
