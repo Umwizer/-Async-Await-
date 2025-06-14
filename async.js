d6d513b9ffee9e69c66c475a7f75742920699955
Umwizer <ruthumwizerwa@gmail.com>
2025-06-14 10:40:34 +0200

    async

--- async.js
+++ async.js
@@ -1,29 +1,43 @@
 //synchronous(synchronous programming)
-
-setTimeout(() => {
-  console.log("1.Preheat the oven to 350 ℉ ");
-}, 1000);
-setTimeout(() => {
-  console.log("2.Chop Half the chocolate into checks & melt is microwave");
-}, 2000);
-setTimeout(() => {
-  console.timeLog("3.Mix butter & sugar");
-}, 1500);
-setTimeout(() => {
-  console.log("4.Beats in the eggs and vanilla for 1-2 minutes.");
-}, 3000);
-setTimeout(() => {
-  console.log("5.Whisk in the melted Chocolate");
-}, 4000);
-setTimeout(() => {
-  console.log(
-    "6.fold in other half of chocolate chunks, the transfer the butter to baking dish"
-  );
-}, 3500);
-
-setTimeout(() => {
-  console.log("7.Bake for 20-25 minutes, then let cool completely");
-}, 1800);
-setTimeout(() => {
-  console.log("8.Slice,serve & enjoy  !");
-}, 500);
+function task1() {
+  setTimeout(() => {
+    console.log("1.Preheat the oven to 350 ℉ ");
+  }, 1000);
+}
+function task2() {
+  setTimeout(() => {
+    console.log("2.Chop Half the chocolate into checks & melt is microwave");
+  }, 2000);
+}
+function task3() {
+  setTimeout(() => {
+    console.timeLog("3.Mix butter & sugar");
+  }, 1500);
+}
+function task4() {
+  setTimeout(() => {
+    console.log("4.Beats in the eggs and vanilla for 1-2 minutes.");
+  }, 3000);
+}
+function task5() {
+  setTimeout(() => {
+    console.log("5.Whisk in the melted Chocolate");
+  }, 4000);
+}
+function task6() {
+  setTimeout(() => {
+    console.log(
+      "6.fold in other half of chocolate chunks, the transfer the butter to baking dish"
+    );
+  }, 3500);
+}
+function task7() {
+  setTimeout(() => {
+    console.log("7.Bake for 20-25 minutes, then let cool completely");
+  }, 1800);
+}
+function task8() {
+  setTimeout(() => {
+    console.log("8.Slice,serve & enjoy  !");
+  }, 500);
+}
